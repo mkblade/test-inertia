70330c6d3fe14cf5ee35dd535152ef7ba3aa80a9
Mykhailo Kostyshyn <mkblade@gmail.com>
2023-05-09 15:36:09 +0200

    page: created Home page component

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { Head } from '@inertiajs/vue3';
+import AppLayout from "@/Layouts/AppLayout.vue";
+
+</script>
+
+<template>
+  <Head title="Home" />
+
+  <AppLayout>
+    Home Page
+  </AppLayout>
+</template>
+
