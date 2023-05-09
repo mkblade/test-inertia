02a0475728b781f9e3349181ce52b898f4534688
Mykhailo Kostyshyn <mkblade@gmail.com>
2023-05-09 20:56:25 +0200

    route: protect article related routes with 'auth' middleware

--- routes/web.php
+++ routes/web.php
@@ -18,13 +18,12 @@ use Inertia\Inertia;
 
 Route::get('/', \App\Http\Controllers\HomeController::class);
 
-Route::resource('articles', \App\Http\Controllers\ArticleController::class);
-
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::resource('articles', \App\Http\Controllers\ArticleController::class);
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
