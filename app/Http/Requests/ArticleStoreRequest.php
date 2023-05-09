66111beeb161a14febefd8ec8a6f0dec9ebc5716
Mykhailo Kostyshyn <mkblade@gmail.com>
2023-05-09 20:57:30 +0200

    validation: use ArticleStoreRequest for validation article fields

---
+++ app/Http/Requests/ArticleStoreRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ArticleStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|min:5|max:255',
+            'content' => 'required|min:10',
+        ];
+    }
+}
